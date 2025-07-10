dae83f889ac6050e36b28dfb6ab0d1f5b6a2c01f
Pratik Thorat <thorat.p@northeastern.edu>
2025-07-09 20:52:43 -0400

    added components to add more umm composability i guess? and exported it to the main App component

--- 01-Static-Page/src/App.jsx
+++ 01-Static-Page/src/App.jsx
@@ -1,23 +1,8 @@
 import "./App.css";
+import Page from "./assets/components/Page";
+
 function App() {
-  return (
-    <main>
-      <img
-        src="src\assets\images\react-icon-small.png"
-        width="40px"
-        alt="React Logo"
-      />
-      <h1> Fun facts about React!</h1>
-      <ul>
-        <li>Was first created in 2013</li>
-        <li>Was orignally created by Jordan Walke</li>
-        <li>Has well over 100k stars on github</li>
-        <li>is maintained by facebook</li>
-        <li>Powers thousands of enterprise apps including mobile apps</li>
-        <li>It gives you high paying jobs</li>
-      </ul>
-    </main>
-  );
+  return <Page />;
 }
 
 export default App;
