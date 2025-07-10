7d5da91a1fc0a12023ef86737f576bafc50de4d7
Pratik Thorat <thorat.p@northeastern.edu>
2025-07-09 20:39:01 -0400

    A non composable version of the react facts application

--- 01-Static-Page/src/App.jsx
+++ 01-Static-Page/src/App.jsx
@@ -1,7 +1,44 @@
 import "./App.css";
-
 function App() {
-  return <div>React Yay!!</div>;
+  return (
+    <main>
+      <img
+        src="src\assets\images\react-icon-small.png"
+        width="40px"
+        alt="React Logo"
+      />
+      <h1> Fun facts about React!</h1>
+      <ul>
+        <li>Was first created in 2013</li>
+        <li>Was orignally created by Jordan Walke</li>
+        <li>Has well over 100k stars on github</li>
+        <li>is maintained by facebook</li>
+        <li>Powers thousands of enterprise apps including mobile apps</li>
+        <li>It gives you high paying jobs</li>
+      </ul>
+    </main>
+  );
 }
 
 export default App;
+
+/*
+import {createRoot} from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+    <main>
+        <img src='react-logo.png' width='40px'/>
+        <h1> Fun facts about React!</h1>
+        <ul>
+            <li>Was first created in 2013</li>
+            <li>Was orignally created by Jordan Walke</li>
+            <li>Has well over 100k stars on github</li>
+            <li>is maintained by facebook</li>
+            <li>Powers thousands of enterprise apps including mobile apps</li>
+            <li>It gives you high paying jobs</li>
+        </ul>
+    </main>
+);
+*/
