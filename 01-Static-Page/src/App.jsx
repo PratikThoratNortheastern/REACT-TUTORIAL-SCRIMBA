1229e01c00203f02ac1afa6be60f2ca087c673b6
Pratik Thorat <thorat.p@northeastern.edu>
2025-07-13 17:12:53 -0400

    Done �

--- 01-Static-Page/src/App.jsx
+++ 01-Static-Page/src/App.jsx
@@ -6,24 +6,3 @@ function App() {
 }
 
 export default App;
-
-/*
-import {createRoot} from 'react-dom/client';
-
-const root = createRoot(document.getElementById('root'));
-
-root.render(
-    <main>
-        <img src='react-logo.png' width='40px'/>
-        <h1> Fun facts about React!</h1>
-        <ul>
-            <li>Was first created in 2013</li>
-            <li>Was orignally created by Jordan Walke</li>
-            <li>Has well over 100k stars on github</li>
-            <li>is maintained by facebook</li>
-            <li>Powers thousands of enterprise apps including mobile apps</li>
-            <li>It gives you high paying jobs</li>
-        </ul>
-    </main>
-);
-*/
--- 01-Static-Page/src/assets/components/Header.jsx
+++ 01-Static-Page/src/assets/components/Header.jsx
@@ -4,17 +4,13 @@ function Header() {
   return (
     <>
       <header className="header">
-        <img
-          src="src\assets\images\react-icon-small.png"
-          className="nav-img"
-          alt="React Logo"
-        />
-        <nav>
-          <ul className="nav-list">
-            <li className="nav-list-items">Pricing</li>
-            <li className="nav-list-items">About</li>
-            <li className="nav-list-items">Content</li>
-          </ul>
+        <nav className="nav-container">
+          <img
+            src="src\assets\images\react-icon-small.png"
+            className="nav-img"
+            alt="React Logo"
+          />
+          <span className="logo-text">ReactFacts</span>
         </nav>
       </header>
     </>
