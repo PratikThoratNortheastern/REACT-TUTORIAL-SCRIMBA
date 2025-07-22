3e7d892496c21a601b6dd91271483ef089ccc2ff
Pratik Thorat <thorat.p@northeastern.edu>
2025-07-21 23:40:16 -0400

    Travel Journal basic setup

---
+++ 02-Data-Driven-React/src/App.jsx
@@ -0,0 +1,7 @@
+import Header from "./assets/components/Header";
+import "./App.css";
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ 02-Data-Driven-React/src/assets/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function () {
+  return (
+    <header>
+      <h1>Hello Travel Journal</h1>
+      <p>Fuck this. Airbnb is expensive</p>
+    </header>
+  );
+}
